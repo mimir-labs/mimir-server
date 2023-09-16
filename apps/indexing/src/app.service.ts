0c042cc629a9deecd8b560c3e643d2a3fc83ef3f
jarvandev <jarvan.z@outlook.com>
2023-09-16 23:30:18 +0800

    add log for disconnect

--- apps/indexing/src/app.service.ts
+++ apps/indexing/src/app.service.ts
@@ -49,6 +49,7 @@ export class AppService {
   private async scan(chainName: string, api: ApiPromise) {
     while (true) {
       if (!api.isConnected) {
+        console.warn('api disconnect, wait 3s to restore');
         await sleep(3000);
         continue;
       }
@@ -61,62 +62,58 @@ export class AppService {
         continue;
       }
 
-      try {
-        const blockNumber = BigInt(blockchain.syncedBlock) + 1n;
+      const blockNumber = BigInt(blockchain.syncedBlock) + 1n;
 
-        const blockHash = await api.rpc.chain.getBlockHash(blockNumber);
+      const blockHash = await api.rpc.chain.getBlockHash(blockNumber);
 
-        const [allRecords, block] = await Promise.all([this.retrieveEvents(api, blockHash), api.rpc.chain.getBlock(blockHash)]);
+      const [allRecords, block] = await Promise.all([this.retrieveEvents(api, blockHash), api.rpc.chain.getBlock(blockHash)]);
 
-        for (let index = 0; index < block.block.extrinsics.length; index++) {
-          const { hash } = block.block.extrinsics[index];
-          // filter the specific events based on the phase and then the
-          // index of our extrinsic in the block
-          const events = allRecords
-            .filter(({ event, phase }) => {
-              return (
-                phase.isApplyExtrinsic &&
-                phase.asApplyExtrinsic.eq(index) &&
-                (api.events.multisig.NewMultisig.is(event) ||
-                  api.events.multisig.MultisigApproval.is(event) ||
-                  api.events.multisig.MultisigExecuted.is(event) ||
-                  api.events.multisig.MultisigCancelled.is(event))
-              );
-            })
-            .map(({ event }) => event);
-
-          events.forEach((event) => {
-            console.log(
-              `retrive event: ${event.section}.${event.method}`,
-              '::',
-              `blockHash: ${blockHash.toHex()}, blockNumber: ${blockNumber.toString()}`,
-              '::',
-              `extrinsicHash: ${hash.toHex()}, extrinsicIndex: ${index}`
+      for (let index = 0; index < block.block.extrinsics.length; index++) {
+        const { hash } = block.block.extrinsics[index];
+        // filter the specific events based on the phase and then the
+        // index of our extrinsic in the block
+        const events = allRecords
+          .filter(({ event, phase }) => {
+            return (
+              phase.isApplyExtrinsic &&
+              phase.asApplyExtrinsic.eq(index) &&
+              (api.events.multisig.NewMultisig.is(event) ||
+                api.events.multisig.MultisigApproval.is(event) ||
+                api.events.multisig.MultisigExecuted.is(event) ||
+                api.events.multisig.MultisigCancelled.is(event))
             );
-          });
+          })
+          .map(({ event }) => event);
 
-          if (events.length > 0) {
-            await this.eventEmitter.emitAsync(
-              'multisig.event',
-              chainName,
-              events.map((event) => ({
-                event,
-                blockHash: blockHash.toHex(),
-                blockNumber: blockNumber.toString(),
-                extrinsicHash: hash.toHex(),
-                extrinsicIndex: index
-              }))
-            );
-          }
-        }
-
-        await this.blockchainRepository.update(blockchain.id, {
-          syncedBlock: blockNumber.toString(),
-          blockHash: blockHash.toHex()
+        events.forEach((event) => {
+          console.log(
+            `retrive event: ${event.section}.${event.method}`,
+            '::',
+            `blockHash: ${blockHash.toHex()}, blockNumber: ${blockNumber.toString()}`,
+            '::',
+            `extrinsicHash: ${hash.toHex()}, extrinsicIndex: ${index}`
+          );
         });
-      } catch (error) {
-        console.error(error);
+
+        if (events.length > 0) {
+          await this.eventEmitter.emitAsync(
+            'multisig.event',
+            chainName,
+            events.map((event) => ({
+              event,
+              blockHash: blockHash.toHex(),
+              blockNumber: blockNumber.toString(),
+              extrinsicHash: hash.toHex(),
+              extrinsicIndex: index
+            }))
+          );
+        }
       }
+
+      await this.blockchainRepository.update(blockchain.id, {
+        syncedBlock: blockNumber.toString(),
+        blockHash: blockHash.toHex()
+      });
     }
   }
 
